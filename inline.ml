cdf9d0228e50340cb52f32a3b79ff530c6013a0c
Ally <bagkingally@gmail.com>
2024-09-25 18:51:28 -0400

    Add support for strong elements

--- inline.ml
+++ inline.ml
@@ -1,11 +1,13 @@
 type inline =
     | Text of string
     | Emphatic of string
+    | Strong of string
 
 let print_inline element =
     match element with
     | Text (text) -> print_endline ("Text: " ^ text)
     | Emphatic (text) -> print_endline ("Emphatic: " ^ text)
+    | Strong (text) -> print_endline ("Strong: " ^ text)
 
 let rec print_inlines elements =
     match elements with
@@ -15,6 +17,7 @@ let rec print_inlines elements =
 type inline_type =
     | PlainText
     | Emphasise
+    | Strengthen
 
 type search_result =
     | NotFound
@@ -36,12 +39,23 @@ let inline_start_index line index =
 
 exception ParseError of string
 
+let inc opt =
+    match opt with
+    | None -> opt
+    | Some (num) -> Some (num + 1)
+
+let blah start_char start_index line =
+    match start_char with
+    |'*' when (line.[start_index + 1] == '*') -> inc (String.index_from_opt line (start_index + 2) '*')
+    |'*' -> String.index_from_opt line (start_index + 1) '*'
+
 let inline_length start_char start_index line =
     match start_char with
-    | '*' -> let result = String.index_from_opt line (start_index + 1) '*' in
+    | '*' -> let result = blah start_char start_index line in
         match result with
-        | None -> raise (ParseError ("Failed to find length for " ^ (String.make 1 start_char) ^ "from " ^ (string_of_int start_index) ^ " in " ^ line))
-        | Some (end_index) -> EndFound (Emphasise, end_index - start_index - 1)
+        | None -> raise (ParseError ("Failed to find length for " ^ (String.make 1 start_char) ^ " from " ^ (string_of_int start_index) ^ " in " ^ line))
+        | Some (end_index) when (line.[end_index - 1] == '*') -> EndFound (Strengthen, end_index - start_index)
+        | Some (end_index) -> EndFound (Emphasise, end_index - start_index)
 
 let sub line start_index length where : string =
     print_endline ("Sub: " ^ where ^ ", start: " ^ (string_of_int start_index) ^ ", length: " ^ (string_of_int length) ^ ", total: " ^ (string_of_int (String.length line)));
@@ -53,9 +67,11 @@ let extract_element start_char start_index line =
         | EndFound (element_type, length) ->
             match element_type with
             | Emphasise ->
-                assert (length >= 0);
-                let element_text = sub line (start_index + 1) length "extract_element" in
+                let element_text = sub line (start_index + 1) (length - 1) "extract_element" in
                   Element (Emphatic (element_text), start_index, length)
+            | Strengthen ->
+                let element_text = sub line (start_index + 2) (length - 3) "extract_element" in
+                  Element (Strong (element_text), start_index, length)
 
 let rec line_to_elements line index =
     let start_result = inline_start_index line index in
@@ -64,16 +80,12 @@ let rec line_to_elements line index =
         | StartFound (char, start_index) -> let element = extract_element char start_index line in
             match element with
             | Element (inline_element, from_index, length) ->
-                let initial_text = Text (sub line index (from_index - 1) "line_to_elements") in
-                        [initial_text; inline_element] @ line_to_elements line (from_index + length + 2)
+                let initial_text = Text (sub line index (from_index - index) "line_to_elements") in
+                        [initial_text; inline_element] @ line_to_elements line (from_index + length + 1)
 
-let rec to_elements lines =
-    match lines with
-    | [] -> []
-    | line::tail -> line_to_elements line 0 @ to_elements tail
+let to_elements line =
+    line_to_elements line 0
 
-
-let s = "Hello *world* woohoo!"
-let l = [s]
-let elements = to_elements l
+let s = "Hello|*world*|**bag**|woohoo!"
+let elements = to_elements s
 let _ = print_inlines elements
