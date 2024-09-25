64ac160a8d33cb2c7a288f25162332f4d33ec72e
Ally <bagkingally@gmail.com>
2024-09-25 16:18:12 -0400

    Create fewer intermediate substrings processing inline elements

--- inline.ml
+++ inline.ml
@@ -2,7 +2,7 @@ type inline =
     | Text of string
     | Emphatic of string
 
-let print_inline element = 
+let print_inline element =
     match element with
     | Text (text) -> print_endline ("Text: " ^ text)
     | Emphatic (text) -> print_endline ("Emphatic: " ^ text)
@@ -22,57 +22,58 @@ type search_result =
     | EndFound of inline_type * int
     | Element of inline * int * int
 
-let rec inline_start_index_impl text inlines =
+let rec inline_start_index_impl line index inlines =
     match inlines with
     | [] -> NotFound
-    | char::tail -> let index = String.index_from_opt text 0 char in
-        match index with
-        | None -> inline_start_index_impl text tail
-        | Some (start_index) -> StartFound (char, start_index)
+    | char::tail -> let inline_index = String.index_from_opt line index char in
+        match inline_index with
+        | None -> inline_start_index_impl line index tail
+        | Some (start_index) -> (assert (start_index >= index));StartFound (char, start_index)
 
-let inline_start_index text =
+let inline_start_index line index =
     let inlines = ['*'] in
-        inline_start_index_impl text inlines
+        inline_start_index_impl line index inlines
 
 exception ParseError of string
 
-let inline_end_index start_char start_index text =
+let inline_length start_char start_index line =
     match start_char with
-    | '*' -> let index = String.index_from_opt text (start_index + 1) '*' in
-        match index with
-        | None -> raise (ParseError ("Failed to find end index for " ^ (String.make 1 start_char) ^ " in " ^ text))
-        | Some (end_index) -> EndFound (Emphasise, end_index)
+    | '*' -> let result = String.index_from_opt line (start_index + 1) '*' in
+        match result with
+        | None -> raise (ParseError ("Failed to find length for " ^ (String.make 1 start_char) ^ "from " ^ (string_of_int start_index) ^ " in " ^ line))
+        | Some (end_index) -> EndFound (Emphasise, end_index - start_index - 1)
 
-let extract_element start_char start_index text =
-    let end_result = inline_end_index start_char start_index text in
+let sub line start_index length where : string =
+    print_endline ("Sub: " ^ where ^ ", start: " ^ (string_of_int start_index) ^ ", length: " ^ (string_of_int length) ^ ", total: " ^ (string_of_int (String.length line)));
+    String.sub line start_index length
+
+let extract_element start_char start_index line =
+    let end_result = inline_length start_char start_index line in
         match end_result with
-        | EndFound (element_type, end_index) ->
+        | EndFound (element_type, length) ->
             match element_type with
-            | Emphasise -> let element_text = String.sub text (start_index + 1) (end_index - start_index - 1) in
-                  Element (Emphatic (element_text), start_index, end_index)
-    
+            | Emphasise ->
+                assert (length >= 0);
+                let element_text = sub line (start_index + 1) length "extract_element" in
+                  Element (Emphatic (element_text), start_index, length)
 
-let rec convert text =
-    let start_index = inline_start_index text in
-        match start_index with
-        | NotFound -> [Text (text)]
-        | StartFound (char, index) -> let element = extract_element char index text in
+let rec line_to_elements line index =
+    let start_result = inline_start_index line index in
+        match start_result with
+        | NotFound -> [Text (String.sub line index ((String.length line) - index))]
+        | StartFound (char, start_index) -> let element = extract_element char start_index line in
             match element with
-            | Element (inline_element, to_index, from_index) -> 
-                let initial_text = Text (String.sub text 0 (to_index - 1)) in
-                    let remaining_text = (String.sub text (from_index + 1) (String.length text - (from_index + 1))) in
-                        [initial_text; inline_element] @ convert remaining_text
+            | Element (inline_element, from_index, length) ->
+                let initial_text = Text (sub line index (from_index - 1) "line_to_elements") in
+                        [initial_text; inline_element] @ line_to_elements line (from_index + length + 2)
 
-
-let rec to_elements texts =
-    match texts with
+let rec to_elements lines =
+    match lines with
     | [] -> []
-    | text::tail -> match text with
-        | Text (text) -> convert text @ to_elements tail
-        | _ -> text::to_elements tail
+    | line::tail -> line_to_elements line 0 @ to_elements tail
 
 
 let s = "Hello *world* woohoo!"
-let l = [Text (s)]
+let l = [s]
 let elements = to_elements l
 let _ = print_inlines elements
