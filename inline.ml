d94e621f8e78bc3e81666f8684f281e25e858549
Ally <bagkingally@gmail.com>
2024-09-25 19:04:17 -0400

    Remove dubious SearchResult type
    Also add inline code

--- inline.ml
+++ inline.ml
@@ -2,12 +2,14 @@ type inline =
     | Text of string
     | Emphatic of string
     | Strong of string
+    | Code of string
 
 let print_inline element =
     match element with
     | Text (text) -> print_endline ("Text: " ^ text)
     | Emphatic (text) -> print_endline ("Emphatic: " ^ text)
     | Strong (text) -> print_endline ("Strong: " ^ text)
+    | Code (text) -> print_endline ("Code: " ^ text)
 
 let rec print_inlines elements =
     match elements with
@@ -18,74 +20,73 @@ type inline_type =
     | PlainText
     | Emphasise
     | Strengthen
-
-type search_result =
-    | NotFound
-    | StartFound of char * int
-    | EndFound of inline_type * int
-    | Element of inline * int * int
+    | Source
 
 let rec inline_start_index_impl line index inlines =
     match inlines with
-    | [] -> NotFound
+    | [] -> (' ', index)
     | char::tail -> let inline_index = String.index_from_opt line index char in
         match inline_index with
         | None -> inline_start_index_impl line index tail
-        | Some (start_index) -> (assert (start_index >= index));StartFound (char, start_index)
+        | Some (start_index) -> (assert (start_index >= index)); (char, start_index)
 
 let inline_start_index line index =
-    let inlines = ['*'] in
+    let inlines = ['*'; '`'] in
         inline_start_index_impl line index inlines
 
 exception ParseError of string
+exception NotImplementedError
 
 let inc opt =
     match opt with
     | None -> opt
     | Some (num) -> Some (num + 1)
 
-let blah start_char start_index line =
+let inline_end_index start_char start_index line =
     match start_char with
     |'*' when (line.[start_index + 1] == '*') -> inc (String.index_from_opt line (start_index + 2) '*')
     |'*' -> String.index_from_opt line (start_index + 1) '*'
+    |'`' -> String.index_from_opt line (start_index + 1) '`'
+    | _ -> raise NotImplementedError
 
 let inline_length start_char start_index line =
-    match start_char with
-    | '*' -> let result = blah start_char start_index line in
+    let result = inline_end_index start_char start_index line in
         match result with
         | None -> raise (ParseError ("Failed to find length for " ^ (String.make 1 start_char) ^ " from " ^ (string_of_int start_index) ^ " in " ^ line))
-        | Some (end_index) when (line.[end_index - 1] == '*') -> EndFound (Strengthen, end_index - start_index)
-        | Some (end_index) -> EndFound (Emphasise, end_index - start_index)
+        | Some (end_index) when (start_char == '*' && line.[end_index - 1] == '*') -> (Strengthen, end_index - start_index)
+        | Some (end_index) when (start_char == '*') -> (Emphasise, end_index - start_index)
+        | Some (end_index) when (start_char == '`') -> (Source, end_index - start_index)
+        | _ -> raise NotImplementedError
 
 let sub line start_index length where : string =
     print_endline ("Sub: " ^ where ^ ", start: " ^ (string_of_int start_index) ^ ", length: " ^ (string_of_int length) ^ ", total: " ^ (string_of_int (String.length line)));
     String.sub line start_index length
 
 let extract_element start_char start_index line =
-    let end_result = inline_length start_char start_index line in
-        match end_result with
-        | EndFound (element_type, length) ->
-            match element_type with
-            | Emphasise ->
-                let element_text = sub line (start_index + 1) (length - 1) "extract_element" in
-                  Element (Emphatic (element_text), start_index, length)
-            | Strengthen ->
-                let element_text = sub line (start_index + 2) (length - 3) "extract_element" in
-                  Element (Strong (element_text), start_index, length)
+    let element_type, length = inline_length start_char start_index line in
+        match element_type with
+        | PlainText -> raise (ParseError "Attempting to extract a plain text element")
+        | Emphasise ->
+            let element_text = sub line (start_index + 1) (length - 1) "extract_element" in
+              (Emphatic (element_text), length)
+        | Strengthen ->
+            let element_text = sub line (start_index + 2) (length - 3) "extract_element" in
+              (Strong (element_text), length)
+        | Source ->
+            let element_text = sub line (start_index + 1) (length - 1) "extract_element" in
+              (Code (element_text), length)
 
 let rec line_to_elements line index =
-    let start_result = inline_start_index line index in
-        match start_result with
-        | NotFound -> [Text (String.sub line index ((String.length line) - index))]
-        | StartFound (char, start_index) -> let element = extract_element char start_index line in
-            match element with
-            | Element (inline_element, from_index, length) ->
-                let initial_text = Text (sub line index (from_index - index) "line_to_elements") in
-                        [initial_text; inline_element] @ line_to_elements line (from_index + length + 1)
+    let start_char, start_index = inline_start_index line index in
+        match start_char with
+        | ' ' -> [Text (String.sub line index ((String.length line) - index))]
+        | _ -> let element, length  = extract_element start_char start_index line in
+                let initial_text = Text (sub line index (start_index - index) "line_to_elements") in
+                        [initial_text; element] @ line_to_elements line (start_index + length + 1)
 
 let to_elements line =
     line_to_elements line 0
 
-let s = "Hello|*world*|**bag**|woohoo!"
+let s = "Hello|*foo*|**bag**|woohoo|`let x = false`|world!"
 let elements = to_elements s
 let _ = print_inlines elements
